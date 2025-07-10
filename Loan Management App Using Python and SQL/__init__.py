27ff3f1a795febc4b54ab94848f04376733512a1
lanryweezy <101323524+lanryweezy@users.noreply.github.com>
2025-07-10 11:10:13 +0100

    Delete Loan Management App Using Python and SQL directory

--- Loan Management App Using Python and SQL/__init__.py
+++
@@ -1,9 +0,0 @@
-from .user import db, User
-from .borrower import Borrower
-from .loan import Loan
-from .payment import Payment
-from .salary import SalaryCalculation
-from .settings import SystemSetting
-
-__all__ = ['db', 'User', 'Borrower', 'Loan', 'Payment', 'SalaryCalculation', 'SystemSetting']
-
